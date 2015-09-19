a90b36e11362442ebbd83b208aa01a85e38020d2
adrianaponte <aaponte@hawk.iit.edu>
2015-09-19 15:17:26 -0500

    Update course.java

--- course.java
+++ course.java
@@ -5,46 +5,46 @@
  */
 
 public class Course {
-  private String courseName;
-  private String instructor1;
-  private String[] students = new String[100];
-  private int numberOfStudents=0;
+  private String courseName;//String that will hold the course name
+  private String instructor1;//string that will hold instructor's name
+  private String[] students = new String[100];//array to hold students
+  private int numberOfStudents=0;//integer that will hold number of students
     
   public Course() {
 	    
 	  }
   
-  public Course(String courseName) {
+  public Course(String courseName) {//adds course name
     this.courseName = courseName;
   }
-  public void addInstructor(String instructor1) {
+  public void addInstructor(String instructor1) {//adds instructor
 	    this.instructor1 = instructor1;
 	  }
-  public void addStudent(String student) {
+  public void addStudent(String student) {//adds new student to array
   
    this.students[numberOfStudents] = student;
    numberOfStudents++;
     
   }
   
-  public String[] getStudents() {
+  public String[] getStudents() {//retrieves students
     return students;
   }
 
-  public int getNumberOfStudents() {
+  public int getNumberOfStudents() {//retrieves student count
     numberOfStudents++;
     return numberOfStudents;
   }  
 
-  public String getCourseName() {
+  public String getCourseName() {//retrieves Course Name
     return courseName;
   }  
   
-  public String getInstructorName() {
+  public String getInstructorName() {//retrieves Instructor Name
 	    return instructor1;
 	  }  
   
-  public Student dropStudent(Student student) {
+  public Student dropStudent(Student student) {//sets student to null
 	  
      student=null;
      return student;
