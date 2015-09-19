585bbbf78bd1458a25e5b88366474b5afb076b7b
adrianaponte <aaponte@hawk.iit.edu>
2015-09-19 15:02:01 -0500

    Update instructor.java

--- instructor.java
+++ instructor.java
@@ -11,47 +11,47 @@ public class Instructor {
 	private double instrucorSalary1; 
 	private double instructorOfficeNum1;
 	
-	 public void setinstructorName(String instructorName) 
+	 public void setinstructorName(String instructorName) //method with String parameter to set Instructor Name
 	 { 
 		 instructorName1 = instructorName; 
 	 } 
 	 
-	 public void setinstructorID(String instructorID) 
+	 public void setinstructorID(String instructorID) //method with String parameter to set Instructor ID
 	 { 
 		 instructorID1 = instructorID; 
 	 } 
 	 
-	 public void setinstrucorSalary(double instrucorSalary) 
+	 public void setinstrucorSalary(double instrucorSalary) //method with double parameter to set Instructor Salary
 	 { 
 		 instrucorSalary1 = instrucorSalary; 
 	 } 
 	 
-	 public void setinstructorOfficeNum(double instructorOfficeNum) 
+	 public void setinstructorOfficeNum(double instructorOfficeNum) ////method with double parameter to set Instructor Office Number
 	 { 
 		 instructorOfficeNum1 = instructorOfficeNum; 
 	 } 
 	 //
 	 
-	 public String getInstructorName() 
+	 public String getInstructorName() //method to return Instructor Name
 	 { 
 		 return instructorName1; 
 	 } 
 	 
-	 public String getinstructorID() 
+	 public String getinstructorID() //method to return Instructor ID
 	 { 
 		 return instructorID1; 
 	 } 
 	 
-	 public double getinstrucorSalary() 
+	 public double getinstrucorSalary() //method to return Instructor Salary
 	 { 
 		 return instrucorSalary1; 
 	 } 
 	 
-	 public double getinstructorOfficeNum() 
+	 public double getinstructorOfficeNum() //method to return Instructor Office Num
 	 { 
 		 return instructorOfficeNum1 ; 
 	 } 
-	 public String changeInstructor(String original, String changed) 
+	 public String changeInstructor(String original, String changed) //method to change instructor name
 	 { 
 		
 		String instructorChange = instructorName1.replaceAll(original, changed); 
