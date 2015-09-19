9888ce99867e3f083e33ce28a6edae4763a582a8
adrianaponte <aaponte@hawk.iit.edu>
2015-09-19 14:23:50 -0500

    Create Student.java

---
+++ Student.java
@@ -0,0 +1,53 @@
+/*
+ * Adrian Aponte
+ * ITM 311
+ * FINAL PROJECT
+ */
+
+public class Student {
+
+	private String studentName1; 
+	private String studentID1; 
+	private double studentGPA1; 
+	private int studentCredits1; 
+
+	 public void setStudentName(String studentName) 
+	 { 
+		 studentName1 = studentName; 
+	 } 
+	 
+	 public void setStudentID(String studentID) 
+	 { 
+		 studentID1 = studentID; 
+	 } 
+	 
+	 public void setStudentGPA(double studentGPA) 
+	 { 
+		 studentGPA1 = studentGPA; 
+	 } 
+	 
+	 public void setStudentCredits(int studentCredits) 
+	 { 
+		 studentCredits1 = studentCredits; 
+	 } 
+	 //
+	 public String getStudentName() 
+	 { 
+		 return studentName1; 
+	 } 
+	 
+	 public String getStudentID() 
+	 { 
+		return studentID1; 
+	 } 
+	 
+	 public double getStudentGPA() 
+	 { 
+		return studentGPA1; 
+	 } 
+	 
+	 public int getStudentCredits() 
+	 { 
+		return studentCredits1; 
+	 } 
+}
